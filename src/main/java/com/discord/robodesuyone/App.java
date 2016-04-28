f021acff743bf454c4855eee2781636177d0fc1e
yikkity <ayliakn@gmail.com>
2016-04-27 20:35:26 -0400

    maven architecture set up

---
+++ src/main/java/com/discord/robodesuyone/App.java
@@ -0,0 +1,13 @@
+package com.discord.robodesuyone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
